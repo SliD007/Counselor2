08b7c98fd98428a79f9071ce89b6b2fb53f6f3ef
SilD007 <whutjiandong75956@gmail.com>
2017-12-19 21:58:32 +0800

    个人中心

--- app/src/main/java/test/example/com/counselor/personal/PersonalActivity.java
+++ app/src/main/java/test/example/com/counselor/personal/PersonalActivity.java
@@ -1,7 +1,11 @@
 package test.example.com.counselor.personal;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.OnClick;
 import test.example.com.counselor.R;
 import test.example.com.counselor.base.BaseActivity;
 
@@ -12,6 +16,15 @@ import test.example.com.counselor.base.BaseActivity;
 public class PersonalActivity extends BaseActivity {
 
 
+    @BindView(R.id.nameTv)
+    TextView nameTv;
+    @BindView(R.id.addressTv)
+    TextView addressTv;
+    @BindView(R.id.lawFirmTv)
+    TextView lawFirmTv;
+    @BindView(R.id.versionTv)
+    TextView versionTv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +33,26 @@ public class PersonalActivity extends BaseActivity {
     @Override
     protected void initContentView(Bundle savedInstanceState) {
         setContentView(R.layout.activity_personal);
+    }
+
+    @OnClick({R.id.rankListRl, R.id.assessRl, R.id.contractRl, R.id.changePwRl, R.id.versionTv})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.rankListRl:
+                toast("rankListRl",false);
+                break;
+            case R.id.assessRl:
+                toast("assessRl",false);
+                break;
+            case R.id.contractRl:
+                toast("contractRl",false);
+                break;
+            case R.id.changePwRl:
+                toast("changePwRl",false);
+                break;
+            case R.id.versionTv:
+                toast("versionTv",false);
+                break;
+        }
     }
 }
