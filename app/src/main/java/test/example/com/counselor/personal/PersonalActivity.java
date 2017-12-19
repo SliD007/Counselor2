0e1d4d202028d98327be835af159f356818e765d
SilD007 <whutjiandong75956@gmail.com>
2017-12-19 21:52:23 +0800

    个人中心

---
+++ app/src/main/java/test/example/com/counselor/personal/PersonalActivity.java
@@ -0,0 +1,24 @@
+package test.example.com.counselor.personal;
+
+import android.os.Bundle;
+
+import test.example.com.counselor.R;
+import test.example.com.counselor.base.BaseActivity;
+
+/**
+ * Created by Sli.D on 2017/12/19.
+ */
+
+public class PersonalActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void initContentView(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_personal);
+    }
+}
