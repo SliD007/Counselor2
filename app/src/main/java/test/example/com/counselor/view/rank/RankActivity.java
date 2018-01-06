0c1eed76fccd0eb70d0e861d7872fbaf0d37e099
SilD007 <whutjiandong75956@gmail.com>
2018-01-06 19:37:59 +0800

    forgetPw

--- app/src/main/java/test/example/com/counselor/view/rank/RankActivity.java
+++ app/src/main/java/test/example/com/counselor/view/rank/RankActivity.java
@@ -3,9 +3,11 @@ package test.example.com.counselor.view.rank;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewStub;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -18,6 +20,7 @@ import test.example.com.counselor.adapter.Common1Adapter;
 import test.example.com.counselor.adapter.ViewHolder1;
 import test.example.com.counselor.base.BaseActivity;
 import test.example.com.counselor.base.MyApplication;
+import test.example.com.counselor.base.MyLvClickListener;
 
 public class RankActivity extends BaseActivity implements IRankView {
 
@@ -29,6 +32,11 @@ public class RankActivity extends BaseActivity implements IRankView {
     ListView rankLv;
 
     List<RankEntity> rankEntities;
+    @BindView(R.id.backTv)
+    TextView backTv;
+    @BindView(R.id.rankVs)
+    ViewStub rankVs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,17 +53,24 @@ public class RankActivity extends BaseActivity implements IRankView {
         setContentView(R.layout.activity_rank);
     }
 
+    private boolean show_vs = false;
+
     private void initDatas() {
-        Log.e("RankActivity","加载数据");
+        Log.e("RankActivity", "加载数据");
         rankEntities = mRankPresenter.getRankEntity();
         rankLv.setAdapter(new Common1Adapter<RankEntity>(this, rankEntities,
-                R.layout.item_rank,onItemClickListener) {
+                R.layout.item_rank, onItemClickListener) {
             @Override
             protected void convertView(ViewHolder1 mViewHolder, View item, RankEntity rankEntity, int position) {
                 TextView tv1 = mViewHolder.getView(R.id.itemRankTv);
                 TextView tv2 = mViewHolder.getView(R.id.itemWorkForTv);
-                tv1.setText("第"+rankEntity.getRank()+"名  "+rankEntity.getName());
-                tv2.setText("服务村社："+rankEntity.getWorkFor());
+                ImageView im = mViewHolder.getView(R.id.nextIm);
+                RelativeLayout titlelBarLl = mViewHolder.getView(R.id.titleRankRl);
+
+                tv1.setText("第" + rankEntity.getRank() + "名  " + rankEntity.getName());
+                tv2.setText("服务村社：" + rankEntity.getWorkFor());
+//                titlelBarLl.setTag(position);
+//                titlelBarLl.setOnClickListener(mClickListener);
             }
         });
         rankLv.setOnItemClickListener(onItemClickListener);
@@ -85,7 +100,7 @@ public class RankActivity extends BaseActivity implements IRankView {
     @OnClick(R.id.backTv)
     public void onClick() {
     }
-    private boolean change = true;
+
     //Item响应回调
     AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
 
@@ -93,14 +108,28 @@ public class RankActivity extends BaseActivity implements IRankView {
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             toast("" + (position), true);
             ImageView im = (ImageView) view.findViewById(R.id.nextIm);
-            if(change){
+            if (!show_vs) {
                 im.setImageResource(R.drawable.u608);
-                change = false;
+                rankVs.setVisibility(View.VISIBLE);
             }else {
                 im.setImageResource(R.drawable.u609);
-                change = true;
+                rankVs.setVisibility(View.GONE);
             }
+            show_vs = !show_vs;
+        }
+    };
+
+    MyLvClickListener mClickListener = new MyLvClickListener() {
+        @Override
+        public void myOnClick(int position, View view) {
+//            toast("" + (position), true);
 
         }
+
+        public void onClick(View v) {   //先响应onclick(权限高) 可以将响应移交出去
+//            myOnClick((Integer) v.getTag(), v);
+            initDatas();
+        }
+
     };
 }
